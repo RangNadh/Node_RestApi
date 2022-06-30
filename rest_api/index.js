30d13fb03b22ba2e1112c6dcdda40bfab9b7d97e
rangolla <ranganadh1509@gmail.com>
2022-06-30 16:01:04 +0530

    commit

--- rest_api/index.js
+++ rest_api/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+//const routes = require('./routes/api');
 
 //setup express app
 const app = express();
+
+app.use('/api', require('./routes/api'));
 
 //listen for request
 app.listen(process.env.port || 4000, function(){
